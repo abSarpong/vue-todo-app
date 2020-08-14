0439c92cc08fc66e1cdd0600fdb42fd42e66ce36
absarpong <mike.sarp.ky@gmail.com>
2020-08-14 14:57:47 +0000

    add edit component

--- src/components/AddToDo.vue
+++ src/components/AddToDo.vue
@@ -1,11 +1,8 @@
 <template>
   <div>
     <form @submit.prevent="onSubmit" action>
-      <div>
-        <label for>Add new todo</label>
-      </div>
       <div class="form-style">
-        <input type="text" v-model="newTodo" />
+        <input type="text" v-model="newTodo" placeholder="What's next?" />
         <button type="submit" class="primary-button">
           + Add ToDo
         </button>
@@ -38,12 +35,29 @@ label {
 }
 input[type="text"] {
   margin: 8px 0 16px 0px;
-  padding: 8px;
+  padding: 8px 25px;
   font-size: 20px;
   margin-right: 8px;
-  width: 334px;
+  width: 300px;
   border: 1px solid #cccccc;
   height: 40px;
+  border-top-left-radius: 50px;
+  border-bottom-left-radius: 50px;
+}
+input[type="text"]:focus {
+  margin: 8px 0 16px 0px;
+  padding: 8px 25px;
+  font-size: 20px;
+  margin-right: 8px;
+  width: 300px;
+  border: 1px solid #cccccc;
+  height: 40px;
+  border-top-left-radius: 50px;
+  border-bottom-left-radius: 50px;
+}
+::placeholder {
+  font-size: 16px;
+  color: #a3a3a3;
 }
 .primary-button {
   padding: 8px 12px;
@@ -53,5 +67,7 @@ input[type="text"] {
   font-weight: 600;
   height: 57px;
   width: 140px;
+  border-top-right-radius: 50px;
+  border-bottom-right-radius: 50px;
 }
 </style>
--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -11,20 +11,35 @@
           />
           {{ todo }}
         </span>
-        <span class="delete-icon">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="#333333"
-            style="width: 16px"
-            @click="deleteTodo"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clip-rule="evenodd"
-            />
-          </svg>
+        <span>
+          <span class="edit-icon">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="#3867d6"
+              style="width: 16px"
+              @click="editTodo"
+            >
+              <path
+                d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"
+              />
+            </svg>
+          </span>
+          <span class="delete-icon">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="#e74c3c"
+              style="width: 16px"
+              @click="deleteTodo"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </span>
         </span>
       </p>
     </div>
@@ -47,6 +62,9 @@ export default {
   methods: {
     deleteTodo: function() {
       this.$emit("delete-todo");
+    },
+    editTodo: function() {
+      this.$emit("edit-todo");
     },
   },
 };
@@ -75,7 +93,13 @@ p {
 }
 .delete-icon {
   padding: 6px 5px 2px 5px;
-  background-color: #e1e1e1;
+  background-color: rgb(248, 182, 175, 0.4);
   border-radius: 50%;
+}
+.edit-icon {
+  padding: 6px 5px 2px 5px;
+  background-color: rgba(199, 224, 243, 0.4);
+  border-radius: 50%;
+  margin-right: 16px;
 }
 </style>
